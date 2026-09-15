7084b654b77d9161595736302ec196e313fc11e1
seanmccarthyns <seanmccarthyns@gmail.com>
2026-09-14 23:02:37 -0400

    fix(tests): retain storage cleanup with CI reporter overrides

--- product/client/scripts/browser-storage-reporter.mjs
+++ product/client/scripts/browser-storage-reporter.mjs
@@ -1,7 +1,7 @@
 import { removeBrowserStorage } from './browser-storage.mjs'
 
 export default class BrowserStorageReporter {
-  constructor({ runId }) { this.runId = runId }
+  constructor({ runId = process.env.AEROLINK_E2E_RUN_ID } = {}) { this.runId = runId }
   // Normal Playwright runs finish web-server teardown before reporter exit.
   async onExit() { removeBrowserStorage(this.runId) }
 }
